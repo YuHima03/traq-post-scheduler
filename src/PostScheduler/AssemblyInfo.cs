fdab963a5052d1c2e9afd02f737ec0ca042775a2
YuHima03 <yuhima03@gmail.com>
2025-05-20 19:56:29 +0900

    🎉 Initial commit

---
+++ src/PostScheduler/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("PostScheduler.Domain")]
+[assembly: InternalsVisibleTo("PostScheduler.Infrastructure")]
+[assembly: InternalsVisibleTo("PostScheduler.WebApp")]
+
