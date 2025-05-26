be56599cc35390157b1aec5aa177f14df143ce96
YuHima03 <yuhima03@gmail.com>
2025-05-26 13:56:16 +0900

    ✨ Domain.Model にレコード型追加

---
+++ src/PostScheduler.Domain/Model/PostedMessageAbstraction.cs
@@ -0,0 +1,8 @@
+﻿namespace PostScheduler.Domain.Model
+{
+    public readonly struct PostedMessageAbstraction
+    {
+        public Guid MessageId { get; init; }
+        public DateTimeOffset PostedAt { get; init; }
+    }
+}
---
+++ src/PostScheduler.Domain/Model/TraqAccessToken.cs
@@ -0,0 +1,9 @@
+﻿namespace PostScheduler.Domain.Model
+{
+    public sealed record class TraqAccessToken(
+        Guid UserId,
+        string AccessToken,
+        DateTimeOffset IssuedAt,
+        DateTimeOffset ExpiresAt
+        );
+}
