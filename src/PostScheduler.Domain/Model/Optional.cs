5d77065a90a86825edc4ee8d6b3a07b44639647f
YuHima03 <yuhima03@gmail.com>
2025-06-01 14:49:14 +0900

    ✨ Optional.Create メソッドを静的クラスに分ける

--- src/PostScheduler.Domain/Model/Optional.cs
+++ src/PostScheduler.Domain/Model/Optional.cs
@@ -8,6 +8,14 @@
 
         public static Optional<T> None => new() { Value = default!, HasValue = false };
 
-        public static Optional<T> Create(T value) => new() { Value = value, HasValue = true };
+        internal static Optional<T> Create(T value) => new() { Value = value, HasValue = true };
+    }
+
+    public static class Optional
+    {
+        public static Optional<T> Create<T>(T value) => Optional<T>.Create(value);
+
+        public static Optional<T?> CreateNotNull<T>(T value) where T : struct
+            => Optional<T?>.Create(new T?(value));
     }
 }
