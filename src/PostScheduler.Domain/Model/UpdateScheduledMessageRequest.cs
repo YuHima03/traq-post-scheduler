d19b1eb2d5997fe2bd8cc3e803c71bffd3229585
YuHima03 <yuhima03@gmail.com>
2025-06-01 14:48:16 +0900

    🔥 PostResult の更新は別操作に分ける

--- src/PostScheduler.Domain/Model/UpdateScheduledMessageRequest.cs
+++ src/PostScheduler.Domain/Model/UpdateScheduledMessageRequest.cs
@@ -4,7 +4,6 @@
         Optional<Guid> UserId,
         Optional<string> Message,
         Optional<bool> IsEmbeddingEnabled,
-        Optional<DateTimeOffset> ScheduledPostingTime,
-        Optional<PostedMessageAbstraction?> PostedMessage
+        Optional<DateTimeOffset> ScheduledPostingTime
         );
 }
